8a578148e916d2296bfc2e63cd7d6e8dcfcd3638
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2016-04-12 10:58:43 +0200

    init commit
    + netpoint media de
    + netpoint media de rta

---
+++ index.php
@@ -0,0 +1,242 @@
+<?php
+
+include 'env.php';
+/**
+  * Get a web file (HTML, XHTML, XML, image, etc.) from a URL.  Return an
+  * array containing the HTTP server response header fields and content.
+*/
+function get_web_page( $url )
+{
+  $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+  $options = array(
+      CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
+      CURLOPT_POST           =>false,        //set to GET
+      CURLOPT_USERAGENT      => $user_agent, //set user agent
+      CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+      CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+      CURLOPT_RETURNTRANSFER => true,     // return web page
+      CURLOPT_HEADER         => false,    // don't return headers
+      CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+      CURLOPT_ENCODING       => "",       // handle all encodings
+      CURLOPT_AUTOREFERER    => false     // set referer on redirect
+  );
+
+  $ch      = curl_init( $url );
+  curl_setopt_array( $ch, $options );
+  $content = curl_exec( $ch );
+  $err     = curl_errno( $ch );
+  $errmsg  = curl_error( $ch );
+  $header  = curl_getinfo( $ch );
+  curl_close( $ch );
+
+  $header['errno']   = $err;
+  $header['errmsg']  = $errmsg;
+  $header['content'] = $content;
+  return $header;
+}
+
+/**
+* Get the VIEWSTATE the EVENTVALIDATION from the value
+*/
+function get_login_data(){
+  //Read a web page and check for errors:
+  $result = get_web_page('https://advertising.criteo.com/login.aspx');
+  if ( $result['errno'] != 0 )
+      die("error: bad url, timeout, redirect loop ". $result['errno']);
+
+  if ( $result['http_code'] != 200 )
+      die("error: no page, no permissions, no service". $result['http_code']);
+
+  $page = $result['content'];
+
+  preg_match('~<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)" />~', $page, $viewstate);
+  preg_match('~<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*?)" />~', $page, $eventValidation);
+
+  $viewstate = $viewstate[1];
+  $eventValidation = $eventValidation[1];
+
+  $data['viewstate']=$viewstate;
+  $data['eventValidation']=$eventValidation;
+  return $data;
+}
+
+/*
+* Access login page with collected data
+*/
+function access($url){
+  $datas = get_login_data();
+  $viewstate = $datas['viewstate'];
+  $eventValidation = $datas['eventValidation'];
+
+  $f = fopen('log.txt', 'w'); // file to write request header for debug purpose
+  $useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.342.3 Safari/533.2';
+  $username = USERNAME;
+  $password = PASSWORD;
+
+  /**
+   *Start Login process
+   */
+  $ch = curl_init($url);
+
+  // curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+  curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_REFERER, $url);
+  curl_setopt($ch, CURLOPT_VERBOSE, 1);
+  curl_setopt($ch, CURLOPT_STDERR, $f);
+  curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+
+  // Collecting all POST fields
+  $postfields = array();
+  $postfields['__EVENTTARGET'] = "";
+  $postfields['__EVENTARGUMENT'] = "";
+  $postfields['__VIEWSTATE'] = $viewstate;
+  $postfields['__EVENTVALIDATION'] = $eventValidation;
+  $postfields['m1$globalContent$ctlLogin$UserName'] = $username;
+  $postfields['m1$globalContent$ctlLogin$Password'] = $password;
+  $postfields['m1$globalContent$ctlLogin$Login'] = 'Log in';
+
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+
+  $content = curl_exec( $ch );
+  $err     = curl_errno( $ch );
+  $errmsg  = curl_error( $ch );
+  $header  = curl_getinfo( $ch );
+
+  curl_close( $ch );
+
+  $header['errno']   = $err;
+  $header['errmsg']  = $errmsg;
+  $header['content'] = $content;
+  return $header;
+}
+
+
+/**
+*login and access destination page
+*/
+function execute($url){
+  $htmlContent = access($url);
+
+  $dom = new \DOMDocument('1.0', 'UTF-8');
+
+  $internalErrors = libxml_use_internal_errors(true);
+
+  //load the html
+  $html = $dom->loadHTML($htmlContent['content']);
+
+  //discard white space
+  $dom->preserveWhiteSpace = false;
+
+  //the table by its tag name
+  $tables = $dom->getElementsByTagName('table');
+
+  //get all rows from the table
+  $rows = $tables->item(0)->getElementsByTagName('tr');
+
+  // get each column by tag name
+  $cols = $rows->item(0)->getElementsByTagName('th');
+  $row_headers = NULL;
+  foreach ($cols as $node) {
+      $row_headers[] = $node->nodeValue;
+  }
+
+  $table = array();
+  //get all rows from the table
+  $rows = $tables->item(0)->getElementsByTagName('tr');
+  foreach ($rows as $row)
+  {
+     // get each column by tag name
+      $cols = $row->getElementsByTagName('td');
+      $row = array();
+      $i=0;
+      foreach ($cols as $node) {
+          $newstr = filter_var($node->nodeValue, FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
+          if($row_headers==NULL)
+              $row[] = $newstr;
+          else
+              $row[$row_headers[$i]] = $newstr;
+          $i++;
+      }
+      $table[] = $row;
+  }
+  /**
+  * Ignore $i = 0 & $i = 1
+  * $i = 0 => NULL
+  * $i = 1 => Total
+  */
+  for ($i=1; $i <=count($table) ; $i++) {
+    if($i!=1 && isset($table[$i])) {
+      foreach ($table[$i] as $key => $value) {
+        $key = trim($key);
+        echo '<pre>';
+        if($key=='Name' || $key=='Impr.' || $key=='Clicks'){
+          echo $key.' : '.$value;
+        }
+        echo '<pre>';
+      }
+    }
+  }
+
+  libxml_use_internal_errors($internalErrors);
+}
+
+function init(){
+  /**
+  * 3 letzte Tage
+  */
+  $current_date = date('Y-m-d');
+  $result_date = new DateTime($current_date);
+
+  $result_date->modify('-1 day');
+  $_min1_date = $result_date->format('Y-m-d');
+  $tag['_min1_date'] = $_min1_date;
+
+  $result_date->modify('-1 day');
+  $_min2_date = $result_date->format('Y-m-d');
+  $tag['_min2_date'] = $_min2_date;
+
+  $result_date->modify('-1 day');
+  $_min3_date = $result_date->format('Y-m-d');
+  $tag['_min3_date'] = $_min3_date;
+
+  echo "Netpoint Media DE\n\n";
+  foreach ($tag as $key => $date) {
+    $exec_date = new DateTime($date);
+    $begindate = $exec_date->format('Y-m-d');
+    $exec_date->modify('+1 day');
+    $enddate = $exec_date->format('Y-m-d');
+
+    $url_netpoint = 'https://advertising.criteo.com/login.aspx?ReturnUrl=%2fstats%2fdefault.aspx%3fbreakdown%3dAffiliate%26history%3dNA%26period%3dYesterday%26begindate%3d'.$begindate.'%26enddate%3d'.$enddate.'%26useIncompleteStats%3dFalse%26networkid%3d119%3faccountid%3d1040&breakdown=Affiliate&history=NA&period=Yesterday&begindate='.$begindate.'&enddate='.$enddate.'&useIncompleteStats=False&networkid=119&accountid=1040';
+    echo "\n\n++++++++ Datum : ".$date." ++++++++";
+    execute($url_netpoint);
+  }
+
+  echo "\n\n++++++++++++++++++++++++++++++++++++++++";
+
+  echo "\n\nNetpoint Media DE RTA";
+  foreach ($tag as $key => $date) {
+    $exec_date = new DateTime($date);
+    $begindate = $exec_date->format('Y-m-d');
+    $exec_date->modify('+1 day');
+    $enddate = $exec_date->format('Y-m-d');
+
+    $url_netpoint_rta = 'https://advertising.criteo.com/login.aspx?ReturnUrl=%2fstats%2fdefault.aspx%3fbreakdown%3dZone%26history%3dNA%26period%3dYesterday%26begindate%3d'.$begindate.'%26enddate%3d'.$enddate.'%26useIncompleteStats%3dFalse%26networkid%3d1329%3faccountid%3d26055&breakdown=Zone&history=NA&period=Yesterday&begindate='.$begindate.'&enddate='.$enddate.'&useIncompleteStats=False&networkid=1329&accountid=26055';
+    echo "\n\n++++++++ Datum : ".$date." ++++++++";
+    execute($url_netpoint_rta);
+  }
+}
+
+/**
+* App
+*/
+init();
+
+?>
